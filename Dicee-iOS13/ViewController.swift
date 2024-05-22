7118c116fea73e3f3368e1aef0f55329a6119fe1
Anil <anilyadavjnt@gmail.com>
2024-05-22 17:38:01 +0530

    Initial Commit

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by  Anil Kumar Yadav on 22/05/2024.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
+    
+ // Anil Image literal was removed from code completion. But copying and     pasting the following will have the same effect, #imageLiteral(
+        
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        
+        
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
+        
+        
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+        
+       
+        
+    }
+    
+}
+
